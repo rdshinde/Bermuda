7c52daebf0f7a4016008f283497f7be28c69c548
Rishikesh Shinde <67017632+rdshinde@users.noreply.github.com>
2022-02-11 15:18:42 +0530

    fix : modal.js

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,12 +1,12 @@
 const overlay = document.querySelector(".overlay");
 const modalAction = document.querySelectorAll(".modal__action");
 const modal = document.querySelectorAll(".modal");
-const openSimplemodal = document.querySelector(".open-simple-modal");
+const openSimpleModal = document.querySelector(".open-simple-modal");
 
-const simplemodal = document.querySelector(".simple-modal");
+const simpleModal = document.querySelector(".simple-modal");
 
-openSimplemodal.addEventListener("click", () => {
-  simplemodal.classList.remove("hide");
+openSimpleModal.addEventListener("click", () => {
+  simpleModal.classList.remove("hide");
   overlay.classList.remove("hide");
 });
 modalAction.forEach((btn) => {
