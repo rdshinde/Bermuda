c6f3fcd6b7c25cfdb022ab2d0c57a77d283b87b0
Rishikesh Shinde <67017632+rdshinde@users.noreply.github.com>
2022-02-12 16:22:19 +0530

    feat: js file link added

---
+++ script.js
@@ -0,0 +1,29 @@
+// Code for Dismissibile Alerts
+const dismissBtns = document.querySelectorAll(".alert__dismiss-btn");
+
+dismissBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.remove();
+  });
+});
+
+// Code for Modals
+const overlay = document.querySelector(".overlay");
+const modalAction = document.querySelectorAll(".modal__action");
+const modal = document.querySelectorAll(".modal");
+const openSimpleModal = document.querySelector(".open-simple-modal");
+
+const simpleModal = document.querySelector(".simple-modal");
+
+openSimpleModal.addEventListener("click", () => {
+  simpleModal.classList.remove("hide");
+  overlay.classList.remove("hide");
+});
+modalAction.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    overlay.classList.add("hide");
+    modal.forEach((ele) => {
+      ele.classList.add("hide");
+    });
+  });
+});
